84d7cdab07dd11d45400a39d98923061ae79f135
Mohammad Anas <ansarianas53665@gmail.com>
2026-04-02 10:56:43 +0530

    injected middleware auth in backend

--- 03_vidtube/src/controllers/user.controllers.js
+++ 03_vidtube/src/controllers/user.controllers.js
@@ -158,8 +158,25 @@ const loginUser = asyncHandler(async (req, res)=>{
 
 const logoutUser = asyncHandler(async (req, res)=>{
   await User.findByIdAndUpdate(
-
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      }
+    },
+    {new: true}
   )
+     const options = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production"
+  }
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"))
+
 })
 
 const refreshAccessToken = asyncHandler(async (req, res)=>{
@@ -186,7 +203,7 @@ const refreshAccessToken = asyncHandler(async (req, res)=>{
 
     const options = {
             httpOnly: true,
-            secure: true
+            secure: process.env.NODE_ENV === "production"
         }
      const {accessToken, newRefreshToken} = await generateAccessAndRefereshTokens(user._id)
     
